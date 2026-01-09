139a915c4b2382e5d8a13317b9b66e09d681b9b4
sba22230 <sba22230@student.cct.ie>
2026-01-09 11:40:27 +0000

    Check in of changes

--- 3_StatnetAnalysis.R
+++ 3_StatnetAnalysis.R
@@ -1,5 +1,3 @@
-
-
 source("0_functions.R")
 
 # Step 1: Load data
@@ -19,17 +17,15 @@ edges_org <- edges
 # reposted_posts <- posts_df |> filter(repost_count > 0, !is.na(uri), !is.na(repost_count))
 #
 if (!is.null(sample_n_posts) && nrow(edges) > 0) {
-  
   edges_filtered <- edges |>
     dplyr::left_join(
       nodes |> dplyr::select(name, repost_count),
       by = c("from" = "name")
     ) |>
     dplyr::filter(repost_count > 0)
-  
+
   sampled_posts <- edges_filtered |>
     slice_sample(n = min(sample_n_posts, nrow(edges_filtered)))
-  
 } else {
   sampled_posts <- posts
 }
@@ -74,11 +70,11 @@ cat("Nodes count:", nrow(nodes), "\n")
 # # Step 5: Enrich nodes with metadata
 metadata <- bind_rows(
   reposts_df |> select(name = handle, display_name, did),
-  posts_df   |> select(name = author_handle, repost_count, like_count)
+  posts_df |> select(name = author_handle, repost_count, like_count)
 ) |>
   distinct(name, .keep_all = TRUE)
 
-nodes <- nodes |> 
+nodes <- nodes |>
   left_join(metadata, by = "name")
 
 #
@@ -88,9 +84,7 @@ nodes <- nodes |>
 # cat("Enriched nodes count:", nrow(nodes), "\n")
 
 # Step 6: Plot enriched network with ggraph
-g2 <- graph_from_data_frame(d = edges,
-                            vertices = nodes,
-                            directed = TRUE)
+g2 <- graph_from_data_frame(d = edges, vertices = nodes, directed = TRUE)
 
 summary_df <- tibble(
   total_users = vcount(g2),
@@ -123,39 +117,38 @@ bluSkynet <- asNetwork(g2)
 summary(bluSkynet)
 
 bluSkynet %v% "vertex.names"
-bluSkynet[, ]
+bluSkynet[,]
 list.edge.attributes(bluSkynet)
 bluSkynet %e% "created_at"
 #as.sociomatrix.sna(bluSkynet, "created_at")
 
 save_graph_svg(
   plot_or_expr = function() {
-sna::gplot(bluSkynet) },
-    filename = "1_bluSkynet_sna.svg"
+    sna::gplot(bluSkynet)
+  },
+  filename = "1_bluSkynet_sna.svg"
 )
 
 save_graph_svg(
   plot_or_expr = function() {
-sna::gplot(bluSkynet, displaylabels = T, mode = "target") # very slow
+    sna::gplot(bluSkynet, displaylabels = TRUE, mode = "target") # very slow
   },
-filename = "2_bluSkynet_sna.svg"
+  filename = "2_bluSkynet_sna.svg"
 )
 
 save_graph_svg(
   plot_or_expr = function() {
-plot(bluSkynet, displaylabels = F)
+    plot(bluSkynet, displaylabels = FALSE)
   },
-filename = "3_bluSkynet_sna.svg"
+  filename = "3_bluSkynet_sna.svg"
 )
 
 save_graph_svg(
   plot_or_expr = function() {
-gplot(bluSkynet,
-      label.cex - 0.2,
-      label.col = "blue",
-      displaylabels = FALSE) # graph is very messy with labels
+    gplot(bluSkynet, label.cex - 0.2, label.col = "blue", displaylabels = FALSE)
+    # graph is very messy with labels
   },
-filename = "4_bluSkynet_sna.svg"
+  filename = "4_bluSkynet_sna.svg"
 )
 
 network.dyadcount(bluSkynet)
@@ -168,48 +161,52 @@ odeg <- degree(bluSkynet, cmode = "outdegree")
 
 save_graph_svg(
   plot_or_expr = function() {
-plot(ideg, odeg, type = "n", xlab = "Incoming", ylab = "Outgoing")
-abline(0, 1, lty = 3)
-text(
-  jitter(ideg), jitter(odeg),
-  labels = network.vertex.names(bluSkynet),
-  cex = 0.5, col = 2
-)
+    plot(ideg, odeg, type = "n", xlab = "Incoming", ylab = "Outgoing")
+    abline(0, 1, lty = 3)
+    text(
+      jitter(ideg),
+      jitter(odeg),
+      labels = network.vertex.names(bluSkynet),
+      cex = 0.5,
+      col = 2
+    )
   },
-filename = "degree_scatter.svg"
-)
-
-save_graph_svg(
-  plot_or_expr = function() {
-hist(ideg, xlab = "Indegree", 
-     main = "Indgree Distribution", prob = TRUE)
-  },
-filename = "indegree_dist.svg"
+  filename = "degree_scatter.svg"
 )
 
 save_graph_svg(
   plot_or_expr = function() {
-hist(odeg, xlab = "Outdegree", 
-     main = "Outdgree Distribution", prob = TRUE)
+    hist(ideg, xlab = "Indegree", main = "Indgree Distribution", prob = TRUE)
   },
-filename = "outdegree_dist.svg"
+  filename = "indegree_dist.svg"
 )
 
 save_graph_svg(
   plot_or_expr = function() {
-gplot(bluSkynet, vertex.cex = (ideg+odeg)^0.5,
-      vertex.sides = 50, label.cex = 0.4,
-      vertex.col = rgb(odeg/max(odeg), 0, ideg/max(ideg)),
-      displaylabels = TRUE, displayisolates = FALSE)
+    hist(odeg, xlab = "Outdegree", main = "Outdgree Distribution", prob = TRUE)
   },
-filename = "5_bluSkynet.svg"
+  filename = "outdegree_dist.svg"
+)
+
+save_graph_svg(
+  plot_or_expr = function() {
+    gplot(
+      bluSkynet,
+      vertex.cex = (ideg + odeg)^0.5,
+      vertex.sides = 50,
+      label.cex = 0.4,
+      vertex.col = rgb(odeg / max(odeg), 0, ideg / max(ideg)),
+      displaylabels = TRUE,
+      displayisolates = FALSE
+    )
+  },
+  filename = "5_bluSkynet.svg"
 )
 
 bet <- betweenness(bluSkynet, gmode = "graph")
 bet
 
-gplot(bluSkynet, vertex.cex = sqrt(bet)/25,
-      gmode = "graph")
+gplot(bluSkynet, vertex.cex = sqrt(bet) / 25, gmode = "graph")
 
 clo <- closeness(bluSkynet, cmode = "suminvundir")
 clo
@@ -253,4 +250,4 @@ sg1 <- ggraph(g2, layout = "stress", bbox = 15) +
   theme_graph()
 plot(sg1)
 save_graph_svg(sg1, "graphLayout_3.svg")
-# tsna to go here 
+# tsna to go here
