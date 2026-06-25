d4a41e0b65c0a7cdf59f03f126953b77695a0644
sba22230 <sba22230@student.cct.ie>
2026-06-25 14:04:46 +0100

    finished a pipeline run from start to finish

--- master_pipeline.R
+++ master_pipeline.R
@@ -13,8 +13,13 @@ source("3_build_graph_and_metrics.R")
 cat("\n=== STEP 4: Visualisation exports ===\n")
 source("4_visualisation.R")
 
-cat("\n=== STEP 5: Temporal / TSNA analysis ===\n")
-#source("Model_functions.R")
-#source("5_NLP_analysis.R")
+cat("\n=== STEP 5: NLP analysis ===\n")
+source("Model_functions.R")
+source("5_NLP_analysis.R")
+
+cat("\n=== STEP 6: Text analysis ===\n")
+
+source("6_TextAnalysis.R")
+
 
 cat("\nPipeline complete.\n")
